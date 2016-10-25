5ed335d8ad19f6eb6504313fe1931383988116a5
gcmsrc <gsarchioni@Giacomos-MacBook-Pro.local>
2016-10-25 10:50:03 +0100

    Add modified_tester.py

---
+++ tools/modified_tester.py
@@ -0,0 +1,66 @@
+#!/usr/bin/pickle
+
+""" 
+
+    This module runs a code similar to tester.py and returns
+    a list of scores.
+
+"""
+
+
+def test_classifier(cv,
+                    clf_best,
+                    features,
+                    labels):
+
+    """
+
+        This function evaluate an algorithm and returns
+        a list with its performance metrics.
+
+        Args:
+            - cv: a cross-validation object
+            - clf_best: a classifier
+            - features, labels: array of features,
+                                labels extracted from
+                                the dataset.
+
+    """
+    
+    true_negatives = 0
+    false_negatives = 0
+    true_positives = 0
+    false_positives = 0
+    for train_idx, test_idx in cv: 
+        features_train = []
+        features_test  = []
+        labels_train   = []
+        labels_test    = []
+        for ii in train_idx:
+            features_train.append( features[ii] )
+            labels_train.append( labels[ii] )
+        for jj in test_idx:
+            features_test.append( features[jj] )
+            labels_test.append( labels[jj] )
+        
+        ### fit the classifier using training set, and test on test set
+        clf_best.fit(features_train, labels_train)
+        predictions = clf_best.predict(features_test)
+        for prediction, truth in zip(predictions, labels_test):
+            if prediction == 0 and truth == 0:
+                true_negatives += 1
+            elif prediction == 0 and truth == 1:
+                false_negatives += 1
+            elif prediction == 1 and truth == 0:
+                false_positives += 1
+            elif prediction == 1 and truth == 1:
+                true_positives += 1
+            
+    try:
+        total_predictions = true_negatives + false_negatives + false_positives + true_positives
+        accuracy = 1.0*(true_positives + true_negatives)/total_predictions
+        precision = 1.0*true_positives/(true_positives+false_positives)
+        recall = 1.0*true_positives/(true_positives+false_negatives)
+        f1 = 2.0 * true_positives/(2*true_positives + false_positives+false_negatives)
+        
+    return [accuracy, precision, recall, f1]
